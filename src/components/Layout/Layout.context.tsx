4c38ee96f4c5a6fd6ab8b3df2e183801e2846a26
Krzysztof Hankiewicz @kjhank <kjhank@gmail.com>
2024-01-24 09:21:18 +0100

    add default value to layout context; update package.json values

--- src/components/Layout/Layout.context.tsx
+++ src/components/Layout/Layout.context.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import { type LayoutContextProps, type LayoutContextProviderProps } from './Layout.types';
 
-export const LayoutContext = createContext({} satisfies LayoutContextProps);
+export const LayoutContext = createContext({ toggleModal: () => { } } satisfies LayoutContextProps);
 
 export const LayoutProvider = ({ children, value }: LayoutContextProviderProps) => (
   <LayoutContext.Provider value={value}>
