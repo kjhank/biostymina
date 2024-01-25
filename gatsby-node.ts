230c1c208c3abf87062cf88fc457ce330287472a
Krzysztof Hankiewicz @kjhank <kjhank@gmail.com>
2024-01-25 22:52:40 +0100

    metadata; package cleanup

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -22,9 +22,9 @@ const templates: Templates = {
   biostymina: path.resolve('./src/templates/product.tsx'),
   generic: path.resolve('./src/templates/generic.tsx'),
   'historia-marki': path.resolve('./src/templates/history.tsx'),
+  'not-found': path.resolve('./src/templates/generic.tsx'),
   odpornosc: path.resolve('./src/templates/articles.tsx'),
   'strona-glowna': path.resolve('./src/templates/home.tsx'),
-  'not-found': path.resolve('./src/templates/generic.tsx'),
 };
 
 const getRequestUrl = (endpoint: string, params?: RequestParams) => {
